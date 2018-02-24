3ec53003794fb4904a3042abc9ec1d7db308e6db
JimmyZhangJW <z1771485029@gmail.com>
2018-02-24 14:36:37 -0800

    tutorial part done

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -9,15 +9,12 @@ import {DomSanitizer} from '@angular/platform-browser';
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
-  public nameArr: Array<String> = ["Dress Yourself"]
-  public urlArr: Array<String> = ["https:\/\/www.youtube.com/embed/iugZbbUvAmI?controls=1"]
-  public descriptionArr: Array<String> = ["description1"]
+  public nameArr: Array<String> = ["Dress Yourself", "Basic SchoolRoles", "Use Polite Words"] 
+  public urlArr: Array<String> = ["https:\/\/www.youtube.com/embed/iugZbbUvAmI?controls=1", "https:\/\/www.youtube.com/embed/RyLzsQKFpB0?controls=1", "https:\/\/www.youtube.com/embed/o1WJnbJDTS0?controls=1"]
+  public descriptionArr: Array<String> = ["description1","description2", "description3"]
   public i = 0;
 //https://www.youtube.com/embed/iugZbbUvAmI?controls=1
   public url: String = "https:\/\/www.youtube.com/embed/iugZbbUvAmI?controls=1";
-  videoURL() {
-    return this.sanitizer.bypassSecurityTrustUrl(this.url);
-  }
 
   // Initialize settings for the chart
   public lineChartData: Array<any> = [
@@ -27,6 +24,22 @@ export class UserDetailsComponent implements OnInit {
   public lineChartOptions:any = {
     responsive: true
   };
+  public onClick(index){
+    this.i = index;
+  }
+
+  public onClickPlus(){
+    if(this.i<this.nameArr.length-1){
+      this.i++;
+    }
+  }
+
+  public onClickMinus(){
+    if(this.i>0){
+      this.i --;
+    }
+  }
+
   public lineChartColors:Array<any> = [
     {
       backgroundColor: 'rgba(77,83,96,0.2)',
@@ -46,42 +59,38 @@ export class UserDetailsComponent implements OnInit {
   public item: any;
   public sub: any;
   public data: any;
-  public id: string;
+
   constructor(private _userService: UserService, private route: ActivatedRoute, private router: Router, public sanitizer: DomSanitizer) {
     this.array = new Array<number>();
-    this.sub = this.route.params.subscribe(params => {
-         this.id = params["id"];},
-       error => {console.log(error);});
   }
 
   ngOnInit() {
-    this.getUser();
   }
 
-  private getUser (): void {
-    this._userService.fetchItem(this.id)
-          .subscribe(
-      item => this.item = item,
-      error => console.log('Error fetching user item!'),
-      () => {
-        this.mapData();
-        this.lineChartData = [{ data: this.data, label: 'Your Brain Activity'}];
-        this.labelData();
-      }
-    )
-  }
+  // private getUser (): void {
+  //   this._userService.fetchItem(this.id)
+  //         .subscribe(
+  //     item => this.item = item,
+  //     error => console.log('Error fetching user item!'),
+  //     () => {
+  //       this.mapData();
+  //       this.lineChartData = [{ data: this.data, label: 'Your Brain Activity'}];
+  //       this.labelData();
+  //     }
+  //   )
+  // }
 
-  private mapData (): void {
-    this.data = this.item.data.split(',').map(Number);
-  }
+  // private mapData (): void {
+  //   this.data = this.item.data.split(',').map(Number);
+  // }
 
-  private labelData (): void {
-    if(this.data.length > 0){
-    this.lineChartLabels.length = this.data.length; }
-    let index = 0;
-    for(let dataPt of this.data){
-      this.lineChartLabels[index] = String(index);
-      index++;
-    }
-  }
+  // private labelData (): void {
+  //   if(this.data.length > 0){
+  //   this.lineChartLabels.length = this.data.length; }
+  //   let index = 0;
+  //   for(let dataPt of this.data){
+  //     this.lineChartLabels[index] = String(index);
+  //     index++;
+  //   }
+  // }
 }
